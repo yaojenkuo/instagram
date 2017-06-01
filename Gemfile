3c542d0296bcaaf8d991e67f23066d56ed43ef5b
yaojenkuo <tonykuoyj@gmail.com>
2017-06-01 12:39:28 +0800

    added acts_as_votable gem

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '>= 3.2'
 gem 'paperclip', '~> 5.1'
 gem 'masonry-rails'
+gem 'acts_as_votable', '~> 0.10.0'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,6 @@
 class PicsController < ApplicationController
-    before_action :find_pic, only: [:show, :edit, :update, :destroy]
+    before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote]
+    before_action :authenticate_user!, except: [:index, :show]
     
     def index
         @pics = Pic.all.order("created_at DESC")
@@ -40,6 +41,10 @@ class PicsController < ApplicationController
         redirect_to root_path
     end
     
+    def upvote
+        @pic.upvote_by current_user
+        redirect_to :back
+    end
     
     
     private
